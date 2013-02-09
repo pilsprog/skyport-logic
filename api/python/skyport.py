595b4eeb8f69bfe90dd77591c093001cc7aca73d
jonathan <jwringstad@gmail.com>
2013-02-09 16:54:57 +0100

    working some more on the python API and the python testbot

--- api/python/skyport.py
+++ api/python/skyport.py
@@ -10,22 +10,38 @@ class SkyportReceiver:
     def __init__(self):
         pass
 
-    def parse_line(self, line):
-        print("parsing line: %s" % line)
+    def parseLine(self, line):
         try:
             json_line = json.loads(line)
-            self._parse_json_packet(json_line)
+            print(json_line)
+            self._parseJsonPacket(json_line)
         except ValueError as e:
-            print("Error decoding JSON packet: '%s'" % e)
+            print("Error decoding JSON packet: %s" % e)
+        except AttributeError as e:
+            print("Invalid message: %s" % e)
 
-    def _parse_json_packet(self, json_packet):
-        pass
+    def _parseJsonPacket(self, json_packet):
+        if json_packet["message"] == "connect":
+            self.cb_handshake_successful()
+        elif json_packet["message"] == "gamestate":
+            if json_packet.turn == 0:
+                self.cb_gamestart(json_packet["turn"], json_packet["map"], json_packet["players"])
+            else:
+                self.cb_gamestate(json_packet["turn"], json_packet["map"], json_packet["players"])    
+        elif json_packet["message"] == "action":
+            self.cb_action()
+        elif json_packet["message"] == "endturn":
+            self.cb_endturn()
+        else:
+            print("unknown message type: '%s'" % json_packet["message"])
         
 class SkyportTransmitter:
     cb_send = None
     def __init__(self, send_function):
         self.cb_send = send_function
-        
+    def sendLoadout(self, primary_weapon, secondary_weapon):
+        self.cb_send(json.dumps({"message":"loadout", "primary-weapon": primary_weapon, 
+                                 "secondary-weapon": secondary_weapon}))
     def sendHandshake(self, name):
         self.cb_send(json.dumps({"message":"connect", "revision": 1, "name": name}))
     
--- util/randomwalker.py
+++ util/randomwalker.py
@@ -1,28 +1,77 @@
 #!/usr/bin/env python2
+
+################## randomwalker.py ####################
+# This is a test-AI for skyport, written in python    #
+# & twisted. It randomly walks around on the board.   #
+# This AI mainly demonstrates the use of the skyport  #
+# python API.                                         #
+# Note that the API is "framework-agnostic", so there #
+# is no need to actually use twisted -- you can just  #
+# use threads and blocking i/o if you want to. All    #
+# you need to provide the skyport API with is a       #
+# function it can use to send data to the socket,     #
+# and you need to call receiver.parse_line() whenever #
+# you receive data on the socket.                     #
+#                                                     #
+# Written by Jonathan Ringstad                        #
+#######################################################
+
 import sys
-sys.path.append("../api/python")
-
 import json
-import skyport
-
 from twisted.internet import reactor
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 
-name = "randomwalker"
-protocol_version = 1
+sys.path.append("../api/python")
+import skyport
+
+NAME = "randomwalker"
 
 class SkyportConnection(LineReceiver):
     receiver = None
     transmitter = None
-    
-    def lineReceived(self, msg):
-        receiver.parse_line(msg)
 
+    def dataReceived(self, data):
+        # for some reason lineReceived() seems to be broken
+        # so I guess we'll have to use this instead. works.
+        self.receiver.parseLine(data)
+        
+    def gotHandshake(self):
+        print("AI got handshake!")
+        
+    def gotError(self, errormessage):
+        print("AI got error: %s" % errormessage)
+        
+    def gotGamestate(self, turnNumber, mapObject, playerList):
+        print("AI got gamestate!")
+        print("turn number: %d" % turnNumber)
+        print("map object: %r" % mapObject)
+        print("players: %r" % playerList)
+        
+    def gotGamestart(self, turnNumber, mapObject, playerList):
+        self.transmitter.sendLoadout("droid", "mortar")
+        print("AI got gamestart!")
+        
+    def gotAction(self, actionType, restData):
+        print("AI got action: %s" % actionType)
+        
+    def gotEndturn(self):
+        print("AI got endturn!")
+        
     def connectionMade(self):
-        receiver = skyport.SkyportReceiver()
-        transmitter = skyport.SkyportTransmitter(self.sendLine)
-        transmitter.sendHandshake(name)
+        self.receiver = skyport.SkyportReceiver()
+        # Set up callbacks for the receiver
+        self.receiver.cb_handshake_successful = self.gotHandshake
+        self.receiver.cb_error = self.gotError
+        self.receiver.cb_gamestate = self.gotGamestate
+        self.receiver.cb_gamestart = self.gotGamestart
+        self.receiver.cb_action = self.gotAction
+        self.receiver.cb_endturn = self.gotEndturn
+        self.transmitter = skyport.SkyportTransmitter(self.sendLine)
+        # the transmitter only needs to know what function to call
+        # to actually send the data to the socket (self.sendLine)
+        self.transmitter.sendHandshake(NAME)
+        # send the initial handshake
 
 class SkyportConnectionFactory(ClientFactory):
     protocol = SkyportConnection
