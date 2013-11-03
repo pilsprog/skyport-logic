716d7334de6eb9a1d00588391dbaf9a5e4708c98
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-11-03 16:53:05 +0100

    remove unneeded constructors

--- src/main/java/skyport/message/action/MineActionMessage.java
+++ src/main/java/skyport/message/action/MineActionMessage.java
@@ -1,9 +1,4 @@
 package skyport.message.action;
 
 public class MineActionMessage extends ActionMessage {
-    
-    public MineActionMessage() {
-        this.type = "mine";
-    }
-
 }
--- src/main/java/skyport/message/action/UpgradeActionMessage.java
+++ src/main/java/skyport/message/action/UpgradeActionMessage.java
@@ -2,10 +2,6 @@ package skyport.message.action;
 
 public class UpgradeActionMessage extends ActionMessage {
     private String weapon;
-    
-    public UpgradeActionMessage(String weapon) {
-       this.weapon = weapon;
-    }
 
     public String getWeaponName() {
         return weapon;
