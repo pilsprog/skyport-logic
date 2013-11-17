2bc5ede386d28a30b834158dcf5932233005ce04
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-11-17 17:38:38 +0100

    refactor and move checks for valid paths

--- src/main/java/skyport/game/weapon/Droid.java
+++ src/main/java/skyport/game/weapon/Droid.java
@@ -88,41 +88,29 @@ public class Droid extends Weapon {
 
     boolean performOneStep(Direction direction) {
         Debug.debug("droid moving '" + direction + "'");
+        if (position.up == null || 
+                position.up.tileType == TileType.SPAWN || 
+                position.up.tileType == TileType.ROCK || 
+                position.up.tileType == TileType.VOID) {
+                return false;
+        }
         switch (direction) {
         case UP:
-            if (position.up == null || position.up.tileType == TileType.SPAWN || position.up.tileType == TileType.ROCK || position.up.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.up;
             return true;
         case DOWN:
-            if (position.down == null || position.down.tileType == TileType.SPAWN || position.down.tileType == TileType.ROCK || position.down.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.down;
             return true;
         case LEFT_UP:
-            if (position.leftUp == null || position.leftUp.tileType == TileType.SPAWN || position.leftUp.tileType == TileType.ROCK || position.leftUp.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.leftUp;
             return true;
         case LEFT_DOWN:
-            if (position.leftDown == null || position.leftDown.tileType == TileType.SPAWN || position.leftDown.tileType == TileType.ROCK || position.leftDown.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.leftDown;
             return true;
         case RIGHT_UP:
-            if (position.rightUp == null || position.rightUp.tileType == TileType.SPAWN || position.rightUp.tileType == TileType.ROCK || position.rightUp.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.rightUp;
             return true;
         case RIGHT_DOWN:
-            if (position.rightDown == null || position.rightDown.tileType == TileType.SPAWN || position.rightDown.tileType == TileType.ROCK || position.rightDown.tileType == TileType.VOID) {
-                return false;
-            }
             position = position.rightDown;
             return true;
         }
