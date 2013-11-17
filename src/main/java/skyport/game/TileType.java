da78fd11b2806605b62a35ffb4068f375ad14e99
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-11-17 17:45:03 +0100

    Add serialized names so it will serialize well
    add method to get a tiletype by the serialized name if we need to.

--- src/main/java/skyport/game/TileType.java
+++ src/main/java/skyport/game/TileType.java
@@ -1,5 +1,37 @@
 package skyport.game;
 
+import skyport.debug.Debug;
+
+import com.google.gson.annotations.SerializedName;
+
 public enum TileType {
-    GRASS, ROCK, SPAWN, VOID, RUBIDIUM, EXPLOSIUM, SCRAP
+    @SerializedName("G") GRASS, 
+    @SerializedName("O") ROCK, 
+    @SerializedName("S") SPAWN, 
+    @SerializedName("V") VOID, 
+    @SerializedName("R") RUBIDIUM, 
+    @SerializedName("E") EXPLOSIUM,
+    @SerializedName("C") SCRAP;
+    
+    public static TileType getTile(String type) {
+        switch (type) {
+        case "G":
+            return GRASS;
+        case "V":
+            return VOID;
+        case "S":
+            return SPAWN;
+        case "E":
+            return EXPLOSIUM;
+        case "R":
+            return RUBIDIUM;
+        case "C":
+            return SCRAP;
+        case "O":
+            return ROCK;
+        default:
+            Debug.error("Error: Unknown tile type '" + type + "'");
+            return VOID;
+        }
+    }
 }
