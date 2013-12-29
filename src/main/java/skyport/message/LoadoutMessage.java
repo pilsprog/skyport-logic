75b8f162e3a517307e8987101c065ff4a107f134
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-12-29 16:18:10 +0100

    loadoutmessage should return the right type

--- src/main/java/skyport/message/LoadoutMessage.java
+++ src/main/java/skyport/message/LoadoutMessage.java
@@ -1,16 +1,32 @@
 package skyport.message;
 
+import skyport.game.weapon.Droid;
+import skyport.game.weapon.Laser;
+import skyport.game.weapon.Mortar;
 import skyport.game.weapon.Weapon;
 
 public class LoadoutMessage extends Message {
     private String primaryWeapon;
     private String secondaryWeapon;
+    
+    private Weapon weapon(String weapon) {
+        switch(weapon.toLowerCase()) {
+        case "droid":
+            return new Droid();
+        case "laser":
+            return new Laser();
+        case "mortar":
+            return new Mortar();
+        default:
+            return new Weapon(weapon);
+        }
+    }
 
     public Weapon getPrimaryWeapon() {
-        return new Weapon(primaryWeapon);
+        return weapon(primaryWeapon);
     }
 
     public Weapon getSecondaryWeapon() {
-        return new Weapon(secondaryWeapon);
+        return weapon(secondaryWeapon);
     }
 }
