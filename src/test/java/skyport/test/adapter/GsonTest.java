3046fb0367a505b72b6eb5f2cf0c659302fad703
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-06-18 14:17:31 +0200

    test using Messages

--- src/test/java/skyport/test/adapter/GsonTest.java
+++ src/test/java/skyport/test/adapter/GsonTest.java
@@ -5,13 +5,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import skyport.adapter.ActionMessageDeserializer;
+import skyport.adapter.MessageDeserializer;
+import skyport.adapter.PointAdapter;
+import skyport.adapter.TileSerializer;
 import skyport.game.Direction;
+import skyport.game.Point;
+import skyport.game.Tile;
+import skyport.message.Message;
 import skyport.message.action.ActionMessage;
 import skyport.message.action.DroidActionMessage;
 
@@ -21,21 +28,26 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
 
 @RunWith(JUnit4.class)
-public class ActionMessageDeserializerTest {
+public class GsonTest {
     private Gson gson;
     
     @Before
     public void initialize() {
+        PropertyConfigurator.configure("log4j.properties");
+        
         gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+            .registerTypeAdapter(Point.class, new PointAdapter())
+            .registerTypeAdapter(Message.class, new MessageDeserializer())
             .registerTypeAdapter(ActionMessage.class, new ActionMessageDeserializer())
+            .registerTypeAdapter(Tile.class, new TileSerializer())
             .create();
     }
     
     @Test
     public void deserializesDroidActionMessage() {
         String json = "{'message':'action', 'type':'droid', 'sequence':['up', 'right-up', 'right-down', 'down']}";
-        ActionMessage message = gson.fromJson(json, ActionMessage.class);
+        Message message = gson.fromJson(json, Message.class);
         assertTrue(message instanceof DroidActionMessage);
        
         DroidActionMessage action = (DroidActionMessage)message;
