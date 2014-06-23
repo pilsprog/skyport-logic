63325b20773f46fcf78e875d4b2ae1f1234fc587
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-06-23 15:55:30 +0200

    update server.sh with new options

--- server.sh
+++ server.sh
@@ -4,4 +4,4 @@ if [ ! -f target/skyport-1.0-SNAPSHOT.jar ]; then
     mvn package
 fi
 
-java -cp target/skyport-1.0-SNAPSHOT.jar skyport.Skyport 54321 0 48000 assets/balanced/high-noon-2p.skyportmap 1000
+java -cp target/skyport-1.0-SNAPSHOT.jar skyport.Skyport --map high-noon-2p.skyportmap
